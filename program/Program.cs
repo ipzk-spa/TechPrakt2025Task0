54117bd0e223ad0b5e55117a3107aa0097f918aa
Степанчук Павло Анатолійович <ipzk241_spa@student.ztu.edu.ua>
2025-01-26 11:53:49 +0200

    Додав консольний проєкт і файл .gitignore

---
+++ program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть перше число:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введiть друге число:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine($"Сума двох чисел: {sum}");
+    }
+}
